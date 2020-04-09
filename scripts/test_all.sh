4aa7aaa30d35b5916c3bce72ed8ccf762bcf8d2d
Luca Bondi <luca.bondi@polimi.it>
2020-04-09 10:23:30 +0200

    Train and test script collection

---
+++ scripts/test_all.sh
@@ -0,0 +1,141 @@
+#!/usr/bin/env bash
+DEVICE=0
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test Xception on FFc23                        |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-Xception_traindb-ff-c23-720-140-140_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test Xception on DFDC                         |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-Xception_traindb-dfdc-35-5-10_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test Xception on FFc23 (triplet)              |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-XceptionST_traindb-ff-c23-720-140-140_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test Xception on DFDC (triplet)               |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-XceptionST_traindb-dfdc-35-5-10_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetB4 on FFc23                  |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetB4_traindb-ff-c23-720-140-140_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetB4 on DFDC                   |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetB4_traindb-dfdc-35-5-10_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetB4 on FFc23 (triplet)        |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetB4ST_traindb-ff-c23-720-140-140_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetB4 on DFDC (triplet)         |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetB4ST_traindb-dfdc-35-5-10_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetAutoAttB4 on FFc23           |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetAutoAttB4_traindb-ff-c23-720-140-140_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetAutoAttB4 on DFDC            |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetAutoAttB4_traindb-dfdc-35-5-10_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetAutoAttB4a on FFc23          |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetAutoAttB4a_traindb-ff-c23-720-140-140_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetAutoAttB4a on DFDC           |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetAutoAttB4a_traindb-dfdc-35-5-10_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetAutoAttB4 on FFc23 (triplet) |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetAutoAttB4ST_traindb-ff-c23-720-140-140_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Test EfficientNetAutoAttB4 on DFDC (triplet)  |"
+echo "-------------------------------------------------"
+python test_model.py \
+--model_path /nas/public/exchange/icpr2020/weights/binclass/net-EfficientNetAutoAttB4ST_traindb-dfdc-35-5-10_face-scale_size-224_seed-41/bestval.pth \
+--testsets ff-c23-720-140-140 dfdc-35-5-10 \
+--device $DEVICE
--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -154,6 +154,79 @@ python train_binclass.py \
 --attention \
 --device $DEVICE
 
+echo ""
+echo "-------------------------------------------------"
+echo "| Train EfficientNetB4 on FFc23 (triplet)        |"
+echo "-------------------------------------------------"
+python train_triplet.py \
+--net EfficientNetB4 \
+--traindb ff-c23-720-140-140 \
+--valdb ff-c23-720-140-140 \
+--face scale \
+--size 224 \
+--batch 12 \
+--lr 1e-5 \
+--valint 500 \
+--patience 10 \
+--maxiter 60000 \
+--seed 41 \
+--attention \
+--embedding \
+--device $DEVICE
+
+python train_binclass.py \
+--net EfficientNetB4ST \
+--traindb ff-c23-720-140-140 \
+--valdb ff-c23-720-140-140 \
+--face scale \
+--size 224 \
+--batch 32 \
+--lr 1e-5 \
+--valint 50 \
+--patience 10 \
+--maxiter 5000 \
+--seed 41 \
+--attention \
+--device $DEVICE \
+--init /nas/public/exchange/icpr2020/weights/triplet/net-EfficientNetB4_traindb-ff-c23-720-140-140_face-scale_size-224_seed-41/bestval.pth
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Train EfficientNetB4 on DFDC (triplet)        |"
+echo "-------------------------------------------------"
+python train_triplet.py \
+--net EfficientNetB4 \
+--traindb dfdc-35-5-10 \
+--valdb dfdc-35-5-10 \
+--face scale \
+--size 224 \
+--batch 12 \
+--lr 1e-5 \
+--valint 500 \
+--patience 10 \
+--maxiter 60000 \
+--seed 41 \
+--attention \
+--embedding \
+--device $DEVICE
+
+python train_binclass.py \
+--net EfficientNetB4ST \
+--traindb dfdc-35-5-10 \
+--valdb dfdc-35-5-10 \
+--face scale \
+--size 224 \
+--batch 32 \
+--lr 1e-5 \
+--valint 50 \
+--patience 10 \
+--maxiter 5000 \
+--seed 41 \
+--attention \
+--device $DEVICE \
+--init /nas/public/exchange/icpr2020/weights/triplet/net-EfficientNetB4_traindb-dfdc-35-5-10_face-scale_size-224_seed-41/bestval.pth
+
 
 echo ""
 echo "-------------------------------------------------"
@@ -181,6 +254,46 @@ echo "| Train EfficientNetAutoAttB4 on DFDC           |"
 echo "-------------------------------------------------"
 python train_binclass.py \
 --net EfficientNetAutoAttB4 \
+--traindb dfdc-35-5-10 \
+--valdb dfdc-35-5-10 \
+--face scale \
+--size 224 \
+--batch 32 \
+--lr 1e-5 \
+--valint 500 \
+--patience 10 \
+--maxiter 30000 \
+--seed 41 \
+--attention \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Train EfficientNetAutoAttB4a on FFc23         |"
+echo "-------------------------------------------------"
+python train_binclass.py \
+--net EfficientNetAutoAttB4a \
+--traindb ff-c23-720-140-140 \
+--valdb ff-c23-720-140-140 \
+--face scale \
+--size 224 \
+--batch 32 \
+--lr 1e-5 \
+--valint 500 \
+--patience 10 \
+--maxiter 30000 \
+--seed 41 \
+--attention \
+--device $DEVICE
+
+
+echo ""
+echo "-------------------------------------------------"
+echo "| Train EfficientNetAutoAttB4a on DFDC          |"
+echo "-------------------------------------------------"
+python train_binclass.py \
+--net EfficientNetAutoAttB4a \
 --traindb dfdc-35-5-10 \
 --valdb dfdc-35-5-10 \
 --face scale \
